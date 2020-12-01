d5c28170a4ef3212ec6b9b10956070e49d92d480
yairgueta <Yair.gueta@mail.huji.ac.il>
2020-12-01 23:58:48 +0200

    Tutorial Shits

---
+++ Assets/PathManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    public Transform[] points;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        for (int i = 0; i < points.Length - 1; i++)
+        {
+            Gizmos.DrawLine(points[i].position, points[i+1].position);
+            Gizmos.DrawIcon(points[i].position, "blendKey");
+        }
+    }
+}
