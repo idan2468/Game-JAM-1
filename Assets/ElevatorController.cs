ee7c47147746288b28aeca179e225c6a343fd63b
IdanS <idans2468@gmail.com>
2020-12-06 01:22:29 +0200

    Created Elevator Obstacle
    Created Elevator Obstacle using LeanTween PingPong

---
+++ Assets/ElevatorController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class ElevatorController : MonoBehaviour
+{
+    [SerializeField]private GameObject startPoint;
+    [SerializeField]private GameObject endPoint;
+    [SerializeField]private float speed = 1.5f;
+
+    private float lastSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.position = startPoint.transform.position;
+        LeanTween.move(gameObject,endPoint.transform.position,2f)
+            .setLoopPingPong(-1)
+            .setEase(LeanTweenType.easeInOutCubic)
+            .setSpeed(speed);
+        lastSpeed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (speed!= lastSpeed)
+        {
+            LeanTween.cancel(gameObject);
+            Start();
+        }
+    }
+}
