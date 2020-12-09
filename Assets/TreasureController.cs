4a156f80312075b29f3dd1dadd7bb32b5c2d8bc6
IdanS <idans2468@gmail.com>
2020-12-10 00:50:18 +0200

    Added treasure script

---
+++ Assets/TreasureController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator animator;
+    private  int animatorOpenParam = Animator.StringToHash("needToOpen");
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool(animatorOpenParam,true);
+        }
+    }
+
+    // private void OnControllerColliderHit(ControllerColliderHit hit)
+    // {
+    //     if (hit.collider.gameObject.CompareTag("Player"))
+    //     {
+    //         animator.SetBool(animatorOpenParam,true);
+    //     }
+    // }
+    //
+    // private void OnCollisionEnter(Collision other)
+    // { 
+    //     if (other.collider.gameObject.CompareTag("Player"))
+    //     {
+    //         animator.SetBool(animatorOpenParam,true);
+    //     }
+    // }
+}
