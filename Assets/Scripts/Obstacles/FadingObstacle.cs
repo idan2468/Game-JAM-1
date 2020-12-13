339c53f864bb6dc23fdf0311f181026948918b42
IdanS <idans2468@gmail.com>
2020-12-13 14:38:52 +0200

    Fix bug in the fading obstacle script
    Fix bug in the fading obstacle script

--- Assets/Scripts/Obstacles/FadingObstacle.cs
+++ Assets/Scripts/Obstacles/FadingObstacle.cs
@@ -4,20 +4,26 @@ public class FadingObstacle : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField]private float fadeTime = 2;
+    private float _thresholdDisappearAlpha = 0.2f; 
     private MeshRenderer _meshRenderer;
     private Color orgColor;
     private LTDescr animation;
+    private BoxCollider _collider;
     void Start()
     {
         TurnOnAnimation();
         _meshRenderer = GetComponent<MeshRenderer>();
+        _collider = GetComponent<BoxCollider>();
         orgColor = _meshRenderer.material.color;
     }
 
     private void TurnOnAnimation()
     {
         animation = LeanTween.alpha(gameObject, 0, fadeTime)
-            .setLoopPingPong(-1).setEase(LeanTweenType.easeInSine);
+            .setLoopPingPong(-1).setEase(LeanTweenType.easeInSine).setOnUpdate((float val) =>
+            {
+                _collider.enabled = val > _thresholdDisappearAlpha;
+            });
     }
     public void ResetAnimation()
     {
